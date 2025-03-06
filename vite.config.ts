67ae747a90fac2000f8a60a8d3400d332df8517c
yabibal <eshetieyabibal@gmail.com>
2025-03-06 19:34:23 +0300

    Update TypeScript configuration and Vite build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   build: {
-    outDir: "dist", // Ensures everything is inside "dist"
-    assetsDir: "assets", // Stores assets in "dist/assets/"
-    sourcemap: false, // Removes .map files from production build
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
     rollupOptions: {
       output: {
         entryFileNames: "assets/[name]-[hash].js",
         chunkFileNames: "assets/[name]-[hash].js",
         assetFileNames: "assets/[name]-[hash].[ext]",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
       },
     },
   },
